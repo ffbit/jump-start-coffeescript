ab11deba4e947a67b36855c598c308ea243e4644
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-12-14 00:02:59 +0200

    Compile from list files from a file

--- gamejam/watch.sh
+++ gamejam/watch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-coffee -w -j script/main.js \
-       -c src/gfx.coffee src/keys.coffee \
-          src/player.coffee src/game.coffee
+xargs coffee -w -b -j script/main.js \
+             -c < files
