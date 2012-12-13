aa3eb35f34b6bc015e64500bccbbc46b1f91752f
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-12-13 23:41:58 +0200

    Adjusted gamejam project for further development.

--- gamejam/watch.sh
+++ gamejam/watch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-coffee -w -j script/main.js -c src/gfx.coffee src/game.coffee
-
+coffee -w -j script/main.js \
+       -c src/gfx.coffee src/keys.coffee \
+          src/player.coffee src/game.coffee
