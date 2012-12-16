0765b046669c889aabf33dafe305d9920d2e3d65
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-12-16 20:32:56 +0200

    Started refactoring of the game module.

--- gamejam/script/main.js
+++ gamejam/script/main.js
@@ -108,73 +108,12 @@ player = {
 };
 
 game = {
-  init: function() {
-    if (!gfx.init()) {
-      alert("Could not set up game canvas!");
-      return;
-    }
-    gfx.clear();
-    return gfx.load(function() {
-      var c, level, level1, makeLevel, rand;
-      c = gfx.ctx;
-      rand = function(max) {
-        return Math.floor(Math.random() * max);
-      };
-      makeLevel = function(ascii) {
-        var asciiMap, col, row, tiles, _i, _len, _results;
-        tiles = {
-          "@": [4, 1],
-          "O": [4, 0],
-          "*": [5, 1],
-          "#": [5, 0]
-        };
-        asciiMap = (function() {
-          var _i, _len, _ref, _results;
-          _ref = ascii.split("\n");
-          _results = [];
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            row = _ref[_i];
-            _results.push(row.split(""));
-          }
-          return _results;
-        })();
-        _results = [];
-        for (_i = 0, _len = asciiMap.length; _i < _len; _i++) {
-          row = asciiMap[_i];
-          _results.push((function() {
-            var _j, _len1, _results1;
-            _results1 = [];
-            for (_j = 0, _len1 = row.length; _j < _len1; _j++) {
-              col = row[_j];
-              _results1.push(tiles[col]);
-            }
-            return _results1;
-          })());
-        }
-        return _results;
-      };
-      level1 = ".............\n...........*.\n....@#@@@@#@.\n.....#....#..\n.....#....#..\n..*..#...@@@.\n..@@@@@...#..\n...#......#..\n...#......#..\n...#......#..\n.OOOOOOOOOOOO";
-      level = makeLevel(level1);
-      return setInterval(function() {
-        var row, tile, x, xPos, y, yPos, _i, _j, _len, _len1;
-        player.update();
-        gfx.clear();
-        for (y = _i = 0, _len = level.length; _i < _len; y = ++_i) {
-          row = level[y];
-          for (x = _j = 0, _len1 = row.length; _j < _len1; x = ++_j) {
-            tile = row[x];
-            if (!tile) {
-              continue;
-            }
-            xPos = x * gfx.tileW;
-            yPos = y * gfx.tileH;
-            gfx.drawSprite(tile[0], tile[1], xPos, yPos);
-          }
-        }
-        return player.render(gfx);
-      }, 33);
-    });
-  }
+  running: false,
+  init: function() {},
+  stop: function() {},
+  start: function() {},
+  reset: function() {},
+  tick: function() {},
+  update: function() {},
+  render: function() {}
 };
-
-game.init();
--- gamejam/src/game.coffee
+++ gamejam/src/game.coffee
@@ -1,65 +1,9 @@
 game =
+  running: false
   init: ->
-    unless gfx.init()
-      alert "Could not set up game canvas!" 
-      return # abort the game
-
-    # Ready to play!
-    gfx.clear()
-    gfx.load ->
-      c = gfx.ctx
-      rand = (max) ->
-        Math.floor Math.random() * max
-      # gfx.drawSprite 0, 0, 50, 50
-
-      makeLevel = (ascii) ->
-        # 1. Define the tile-to-symbol map
-        tiles =
-          "@": [4, 1]
-          "O": [4, 0]
-          "*": [5, 1]
-          "#": [5, 0]
-        # 2. Cut up the ASCII
-        asciiMap = (row.split "" for row in ascii.split "\n")
-        # 3. Map the character to their tiles
-        (for row in asciiMap
-          for col in row
-            tiles[col])
-
-      level1 = """
-        .............
-        ...........*.
-        ....@#@@@@#@.
-        .....#....#..
-        .....#....#..
-        ..*..#...@@@.
-        ..@@@@@...#..
-        ...#......#..
-        ...#......#..
-        ...#......#..
-        .OOOOOOOOOOOO
-      """
-
-      # Create a map from the ASCII
-      level = makeLevel level1
-
-      setInterval ->
-        # run the things
-        player.update()
-
-        gfx.clear()
-
-        # Draw the level
-        for row, y in level
-          for tile, x in row
-            continue unless tile
-            xPos = x * gfx.tileW
-            yPos = y * gfx.tileH
-            gfx.drawSprite tile[0], tile[1],
-                           xPos, yPos
-
-        player.render gfx
-      , 33
-
-# Start the game running
-game.init()
+  stop: ->
+  start: ->
+  reset: ->
+  tick: ->
+  update: ->
+  render: ->
