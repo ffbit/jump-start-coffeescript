eeaae3d63cdac8058f755a5449b0b8e291fd6d70
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-12-26 21:33:48 +0200

    Sounds.

--- gamejam/watch.sh
+++ gamejam/watch.sh
@@ -3,6 +3,7 @@
 xargs coffee -w -b -j script/main.js \
              -c << EOF
                 src/_utils.coffee
+                src/sound.coffee
                 src/gfx.coffee
                 src/keys.coffee
                 src/entities/
